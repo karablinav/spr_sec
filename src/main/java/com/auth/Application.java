2d0aa032ff075d6ce1fe8e22cdf1f3c6250aa468
karablinav <karablinav@gmail.com>
2018-09-07 10:37:49 +0300

    global auth with AuthenticationManagerBuilder added NoOpPasswordEncoder for Spring 5

--- src/main/java/com/auth/Application.java
+++ src/main/java/com/auth/Application.java
@@ -1,11 +1,30 @@
 package com.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @SpringBootApplication
 public class Application {
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user")
+                .password("password")
+                .roles("USER");
+    }
+
+    @SuppressWarnings("deprecation")
+    @Bean
+    public static NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
 }
